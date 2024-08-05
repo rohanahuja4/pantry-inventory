d1397bad3e74f98f6024ed3185c880c8ffbf8b74
Rohan Ahuja <rohanahuja@Rohans-MacBook-Pro-2.local>
2024-08-04 23:08:22 -0400

    Updated firebase.js

--- inventory-management-app/firebase.js
+++ inventory-management-app/firebase.js
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-WSdPecnjcdm1btn2lGUVIeneVy7iYnM",
-  authDomain: "inventory-management-app-eaa1c.firebaseapp.com",
-  projectId: "inventory-management-app-eaa1c",
-  storageBucket: "inventory-management-app-eaa1c.appspot.com",
-  messagingSenderId: "696016747507",
-  appId: "1:696016747507:web:0ba15905af474e6482b086",
-  measurementId: "G-VKM3C9J7Q4",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
